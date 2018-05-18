64d7e56e72c04131099f345837df67be1fb33ce8
ProPillow <wilsonshum96@hotmail.com>
2018-05-18 02:51:56 -0700

    scraped data and bar graph the data

--- graphData.py
+++ graphData.py
@@ -1,7 +1,39 @@
 import matplotlib.pyplot as plt
+import csv
+from collections import defaultdict
+import collections
 
-x = [1,2,3]
-y = [5,7,4]
-plt.plot(x,y)
+columns = defaultdict(list)
+
+with open('data.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        for i in range(len(row)):
+            columns[i].append(row[i])
+    columns = dict(columns)
+
+print(columns)
+
+allWords = []
+
+for comment in columns[1]:
+    words = comment.split()
+    for word in words:
+        allWords.append(word)
+        # print(word)
+
+allWords = map(str.lower, allWords)
+
+counter = collections.Counter(allWords)
+print(counter)
+print(counter.keys())
+print(counter.values())
+
+x = list(counter.keys())[0:10]
+y = list(counter.values())[0:10]
+plt.title("Top 10 Words vs #times used")
+plt.xlabel("words")
+plt.ylabel("#times")
+plt.bar(x, y)
 
 plt.show()
--- scrapReddit.py
+++ scrapReddit.py
@@ -2,6 +2,7 @@
 
 from credential import reddit
 import praw
+from praw.models import MoreComments
 import pandas as pd
 import datetime as dt
 
@@ -10,31 +11,29 @@ print(reddit.user.me())  # confirmation
 subreddit = reddit.subreddit('leagueoflegends')
 print(subreddit.display_name)
 
-top_subreddit = subreddit.hot(limit = 50)
-print(top_subreddit)
+hot_subreddit = subreddit.top(limit=1)
 
-topics_dict = { "title":[], \
-                "score":[], \
-                "id":[], "url":[], \
-                "comms_num": [], \
-                "created": [], \
-                "body":[]}
+topics_dict = {
+    "likes": [], \
+    "popular": []}
 
-for submission in top_subreddit:
-    topics_dict["title"].append(submission.title)
-    topics_dict["score"].append(submission.score)
-    topics_dict["id"].append(submission.id)
-    topics_dict["url"].append(submission.url)
-    topics_dict["comms_num"].append(submission.num_comments)
-    topics_dict["created"].append(submission.created)
-    topics_dict["body"].append(submission.selftext)
+for submission in hot_subreddit:
+    top_level_comments = list(submission.comments)
+    for top_level_comment in top_level_comments:
+        if isinstance(top_level_comment, MoreComments):
+            continue
+        topics_dict["popular"].append(top_level_comment.body)
+        topics_dict["likes"].append(top_level_comment.score)
+    # all_comments = submission.comments.list()
+    # for comment in all_comments:
+    #     topics_dict["all"].append(comment.body)
 
-topics_data = pd.DataFrame(topics_dict)
+topics_data = pd.DataFrame(data=topics_dict)
 
-def get_date(created):
-    return dt.datetime.fromtimestamp(created)
+# def get_date(created):
+#     return dt.datetime.fromtimestamp(created)
+#
+# timestamp = topics_data["created"].apply(get_date)
+# topics_data = topics_data.assign(timestamp = timestamp)
 
-timestamp = topics_data["created"].apply(get_date)
-topics_data = topics_data.assign(timestamp = timestamp)
-
-topics_data.to_csv('result.csv', index = False)
+topics_data.to_csv('data.csv', index=False)
